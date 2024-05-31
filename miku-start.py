31a631a747f2b59a434ae5919bbae9016152174f
sam <77495892+1jammer1@users.noreply.github.com>
2024-05-31 16:32:15 +0000

    miku

---
+++ miku-start.py
@@ -0,0 +1,26 @@
+import requests
+import time
+import json
+
+papi = "http://127.0.0.1:11434/api/pull"
+api = "http://127.0.0.1:11434/api/generate"
+pull = { "name": "bringusai/miku" }
+ques = {'model': "bringusai/miku"}
+
+
+
+print("MikuAI reqiures Ollama please install it or have a heart attack")
+time.sleep(2)
+
+requests.post(papi, json = pull)
+
+input = input("input here mr. tester man: ")
+
+ques['prompt'] = input
+ques['format'] = "json"
+ques['stream'] = False
+
+mikuattack = json.dumps(ques)
+
+x = requests.post(api, json = mikuattack)
+
