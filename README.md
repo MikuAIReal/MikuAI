b15bb4533474eba131542a7a13de072d4edcbda6
sam <epic.capoj@gmail.com>
2024-06-05 07:07:53 -0400

    shrek 2

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 worm
 
 I have made this project to emulate miku and make her do things with an interactive display :))))
+
+## kill counter
+
+
+2 mikus have died this week
+
+
+7 in total
--- miku-start.py
+++ miku-start.py
@@ -6,6 +6,7 @@ papi = "http://127.0.0.1:11434/api/pull"
 api = "http://127.0.0.1:11434/api/generate"
 pull = { "name": "bringusai/miku" }
 ques = {'model': "bringusai/miku"}
+headers = {"Content-Type": "application/json"}
 
 
 
@@ -13,17 +14,34 @@ print("MikuAI reqiures Ollama please install it or have a heart attack")
 
 time.sleep(2)
 
-requests.post(papi, json = pull)
+
+
+#y = requests.post(papi, json = pull)
+
+#print(y)
 
 input = input("input here mr. tester man: ")
 
 ques['prompt'] = input
-ques['format'] = "json"
-ques['stream'] = False
+#ques['format'] = "json"
+#ques['stream'] = false
 
 mikuattack = json.dumps(ques)
 
-x = requests.post(api, json = mikuattack)
+print(mikuattack)
+
+#mikuattack3 = {"model": "bringusai/miku", "prompt": "hi", "stream": False}
+
+
+mikuattack2 = json.loads(mikuattack)
+
+print(type(mikuattack2))
+
+x = requests.post("http://127.0.0.1:11434/api/generate", json = mikuattack2, headers = headers)
+
+print(x.text)
+print(x)
+
 
 
 
