8def508c77d7d09a2bd88584cd6803a2056c7386
sam <epic.capoj@gmail.com>
2024-06-14 15:08:19 -0400

    commit

--- miku-start.py
+++ miku-start.py
@@ -1,6 +1,23 @@
 import requests
 import time
 import json
+import genshin
+engine = genshin.Genshin()
+engine.set_voice('miku_en')
+
+def ask():
+    input = input("input here mr. tester man: ")
+    ques['prompt'] = input
+    #ques['format'] = "json"
+    ques['stream'] = False
+
+    mikuattack = json.dumps(ques)
+
+    global x
+    
+    x = requests.post("http://127.0.0.1:11434/api/generate", json = mikuattack2, headers = headers)
+
+
 
 papi = "http://127.0.0.1:11434/api/pull"
 api = "http://127.0.0.1:11434/api/generate"
@@ -14,34 +31,22 @@ print("MikuAI reqiures Ollama please install it or have a heart attack")
 
 time.sleep(2)
 
+ask()
 
 
-#y = requests.post(papi, json = pull)
-
-#print(y)
-
-input = input("input here mr. tester man: ")
-
-ques['prompt'] = input
-#ques['format'] = "json"
-ques['stream'] = False
-
-mikuattack = json.dumps(ques)
-
-print(mikuattack)
-
-#mikuattack3 = {"model": "bringusai/miku", "prompt": "hi", "stream": False}
-
-
-mikuattack2 = json.loads(mikuattack)
-
-print(type(mikuattack2))
-
-x = requests.post("http://127.0.0.1:11434/api/generate", json = mikuattack2, headers = headers)
+x1 = json.loads(x)
 
 print(x.text)
 print(x)
+time.sleep(2)
+mikuoutput = x.text["response"]
 
+index = mikuoutput.find('s')
+if index == -1:
+    print("Not found.")
+else:
+    time.sleep(index / 10)
+    mikuemo = happy
 
 
 
